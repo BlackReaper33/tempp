5c1575594bf6d5f05b60ed1ee2d574b53b6faac5
Black Reaper <84635243+BlackReaper33@users.noreply.github.com>
2022-04-26 03:22:53 +0500

    .

---
+++ web/src/components/items/message/MessageContent.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Box, Flex, Image, Text } from '@chakra-ui/react';
+import { Message as MessageResponse } from '../../../lib/api/models';
+
+interface MessageProps {
+  message: MessageResponse;
+}
+
+export const MessageContent: React.FC<MessageProps> = ({ message: { attachment, text, createdAt, updatedAt } }) => {
+  if (attachment) {
+    const { filetype, url } = attachment;
+    if (filetype.startsWith('image/')) {
+      return (
+        <Box boxSize="sm" my={'2'} h={'full'}>
+          <Image fit={'contain'} src={url} alt={''} borderRadius="md" />
+        </Box>
+      );
+    } else if (filetype.startsWith('audio/')) {
+      return (
+        <Box my={'2'}>
+          <audio controls>
+            <source src={url} type={filetype} />
+          </audio>
+        </Box>
+      );
+    }
+  }
+  return (
+    <Flex alignItems={'center'}>
+      <Text>{text}</Text>
+      {createdAt !== updatedAt && (
+        <Text fontSize={'10px'} ml={'1'} color={'labelGray'}>
+          (edited)
+        </Text>
+      )}
+    </Flex>
+  );
+};
